5ca098aa27a3a46345391b905b3cb1c90868c04f
JohnnyOhms <officialjohn662@gmail.com>
2023-06-23 13:05:52 -0400

    updated mysql database

--- server.dev.js
+++ server.dev.js
@@ -2,7 +2,9 @@ const express = require("express");
 const statusCode = require("http-status");
 const cors = require("cors");
 const Router = require("./routes/route");
+const authRouter = require("./routes/auth");
 const notFoundMiddleware = require("./middlewares/notfound");
+const errorHandlerMiddleware = require("./middlewares/errorHandler");
 const upload = require("./config/upload");
 const bodyParser = require("body-parser");
 const session = require("express-session");
@@ -28,7 +30,9 @@ app.post("/api/upload", upload.single("imgUpload"), (req, res, next) => {
 });
 
 app.use("/api/", Router);
+app.use("/api/", authRouter);
 app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 app.use(
   session({
@@ -40,9 +44,15 @@ app.use(
 );
 
 app.listen(9000, () => {
-  console.log("running on PORT 9000");
   db.connect((err) => {
     if (err) return console.log("failed to connect db", err);
+    db.query("SHOW DATABASES", (err, result) => {
+      if (err) {
+        return console.log("failed to display db");
+      }
+      // console.log(result);
+    });
+    console.log("running on PORT 9000");
     console.log("connected to db");
   });
 });
