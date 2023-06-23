1338f60e3813b31aa518568175796e77dd685fdf
JohnnyOhms <officialjohn662@gmail.com>
2023-06-23 17:09:24 -0400

    utils folder added

---
+++ config/util.js
@@ -0,0 +1,24 @@
+const jw = require("jsonwebtoken");
+const fs = require("fs");
+const path = require("path");
+
+const JwtToken = {
+  signToken: (object) => {
+    const privateKey = fs.readFileSync(
+      path.join(__dirname, "/id_rsa_private.pem"),
+      "utf-8"
+    );
+    return jw.sign(
+      object,
+      privateKey,
+      { algorithm: "RS256" },
+      process.env.JSON_EXPIRE
+    );
+  },
+  verifyToken: (token) => {
+    const publicKey = fs.readFileSync("./id_rsa_public.pem", "utf-8");
+    jw.verify(token, publicKey, { algorithms: ["RS256"] });
+  },
+};
+
+module.exports = { JwtToken };
