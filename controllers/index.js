e71b8e6b1c5c95b9894be70068fd94a82732f1c4
JohnnyOhms <officialjohn662@gmail.com>
2023-06-30 13:33:23 -0400

    extract text api

--- controllers/index.js
+++ controllers/index.js
@@ -1,23 +1,53 @@
 const statusCode = require("http-status");
 const Tesseract = require("tesseract.js");
 const fs = require("node:fs");
+const db = require("../config/db");
+const { BadRequestError } = require("../errors");
 
 const dashboard = (req, res, next) => {
+  console.log(req.user);
   res.status(statusCode.OK).send("dashboard page");
 };
 
-const extractText = (req, res) => {
-  const img = fs.readFileSync("./image2.png");
-  Tesseract.recognize(img, "eng", { logger: (m) => console.log(m) })
-    .then((d) => {
-      console.log(d);
-      //   res.send(`<p>${d.data.hocr}</p>`);
-      res.status(statusCode.CREATED).send(`<p>${d}</p>`);
-    })
-    .catch((err) => console.log(err));
+const extractText = (req, res, next) => {
+  db.query(
+    `SELECT * FROM uploads WHERE userId = ?`,
+    [req.user.userId],
+    (err, result) => {
+      if (err) {
+        return next(new BadRequestError("failed to upload"));
+      }
+      const img = fs.readFileSync(`./uploads/${result[0].toString()}`);
+      Tesseract.recognize(img, "eng", {})
+        .then((d) => {
+          return res
+            .status(statusCode.CREATED)
+            .json({ sucess: true, text: d.data.text });
+        })
+        .catch((err) => next(new BadRequestError("failed to extract text ")));
+    }
+  );
+};
+
+const uploadFile = (req, res, next) => {
+  db.query(
+    `INSERT INTO upoads ('userId', 'file') VALUES (?, ?)`,
+    [req.user.userId, req.file.filename],
+    (err, result) => {
+      if (err) {
+        next(new BadRequestError("failed to upload"));
+      }
+      return res.status(statusCode.CREATED).json({
+        sucess: true,
+        file: { name: req.file.originalname, size: req.file.size },
+      });
+    }
+  );
+  res.send("upload sucessfully");
 };
 
 module.exports = {
   extractText,
   dashboard,
+  uploadFile,
 };
