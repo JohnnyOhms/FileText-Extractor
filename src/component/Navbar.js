4d3b5bed899f349805597f61f114f90d94e8e461
JohnnyOhms <officialjohn662@gmail.com>
2023-07-07 07:34:00 -0400

    navbar updated

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,44 +1,15 @@
 import * as React from "react";
-import { styled, alpha } from "@mui/material/styles";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
-import InputBase from "@mui/material/InputBase";
-import Badge from "@mui/material/Badge";
 import MenuItem from "@mui/material/MenuItem";
 import Menu from "@mui/material/Menu";
-import MenuIcon from "@mui/icons-material/Menu";
-import SearchIcon from "@mui/icons-material/Search";
 import AccountCircle from "@mui/icons-material/AccountCircle";
-import MailIcon from "@mui/icons-material/Mail";
-import NotificationsIcon from "@mui/icons-material/Notifications";
-import MoreIcon from "@mui/icons-material/MoreVert";
-
-const SearchIconWrapper = styled("div")(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: "100%",
-  position: "absolute",
-  pointerEvents: "none",
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: "inherit",
-  "& .MuiInputBase-input": {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create("width"),
-    width: "100%",
-    [theme.breakpoints.up("md")]: {
-      width: "20ch",
-    },
-  },
-}));
+import LoginIcon from "@mui/icons-material/Login";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import MenuIcon from "@mui/icons-material/Menu";
 
 export const NavBar = () => {
   const [anchorEl, setAnchorEl] = React.useState(null);
@@ -105,11 +76,9 @@ export const NavBar = () => {
     >
       <MenuItem>
         <IconButton size="large" aria-label="show 4 new mails" color="inherit">
-          <Badge badgeContent={4} color="error">
-            <MailIcon />
-          </Badge>
+          <TwitterIcon />
         </IconButton>
-        <p>Messages</p>
+        <p>About Me</p>
       </MenuItem>
       <MenuItem>
         <IconButton
@@ -117,11 +86,9 @@ export const NavBar = () => {
           aria-label="show 17 new notifications"
           color="inherit"
         >
-          <Badge badgeContent={17} color="error">
-            <NotificationsIcon />
-          </Badge>
+          <LoginIcon />
         </IconButton>
-        <p>Notifications</p>
+        <p>Login</p>
       </MenuItem>
       <MenuItem onClick={handleProfileMenuOpen}>
         <IconButton
@@ -133,7 +100,7 @@ export const NavBar = () => {
         >
           <AccountCircle />
         </IconButton>
-        <p>Profile</p>
+        <p>Dashboard</p>
       </MenuItem>
     </Menu>
   );
@@ -142,55 +109,51 @@ export const NavBar = () => {
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="open drawer"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
           <Typography
             variant="h6"
             noWrap
             component="div"
-            sx={{ display: { xs: "none", sm: "block" } }}
+            sx={{
+              ml: { xs: "2px", sm: "40px" },
+              fontWeight: "800",
+              fontFamily: "unset",
+            }}
           >
-            MUI
+            estrarre il testo
           </Typography>
           <Box sx={{ flexGrow: 1 }} />
+
           <Box sx={{ display: { xs: "none", md: "flex" } }}>
-            <IconButton
-              size="large"
-              aria-label="show 4 new mails"
-              color="inherit"
+            <Typography variant="h6" sx={{ margin: "0 7px", fontSize: "17px" }}>
+              <IconButton size="small" color="inherit">
+                <TwitterIcon fontSize="16px" />
+              </IconButton>
+              About
+            </Typography>
+            <Typography variant="h6" sx={{ margin: "0 7px", fontSize: "17px" }}>
+              <IconButton size="small" color="inherit">
+                <LoginIcon fontSize="16px" />
+              </IconButton>
+              Login
+            </Typography>
+            <Typography
+              variant="h6"
+              sx={{
+                margin: "0 7px",
+                fontSize: "17px",
+                borderRadius: "1rem",
+                padding: "0 12px 0 6px",
+                background: "#106dd8",
+                color: "white",
+              }}
             >
-              <Badge badgeContent={4} color="error">
-                <MailIcon />
-              </Badge>
-            </IconButton>
-            <IconButton
-              size="large"
-              aria-label="show 17 new notifications"
-              color="inherit"
-            >
-              <Badge badgeContent={17} color="error">
-                <NotificationsIcon />
-              </Badge>
-            </IconButton>
-            <IconButton
-              size="large"
-              edge="end"
-              aria-label="account of current user"
-              aria-controls={menuId}
-              aria-haspopup="true"
-              onClick={handleProfileMenuOpen}
-              color="inherit"
-            >
-              <AccountCircle />
-            </IconButton>
+              <IconButton size="small" color="inherit">
+                <AccountCircle fontSize="16px" />
+              </IconButton>
+              Dashboard
+            </Typography>
           </Box>
+
           <Box sx={{ display: { xs: "flex", md: "none" } }}>
             <IconButton
               size="large"
@@ -200,7 +163,7 @@ export const NavBar = () => {
               onClick={handleMobileMenuOpen}
               color="inherit"
             >
-              <MoreIcon />
+              <MenuIcon />
             </IconButton>
           </Box>
         </Toolbar>
