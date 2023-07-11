70e5cb35bd025f1d8ecd5968e5646f1e6fcae682
JohnnyOhms <officialjohn662@gmail.com>
2023-07-11 19:33:01 -0400

    sectionTwo component added

--- src/component/Dash-SectionTwo.js
+++ src/component/Dash-SectionTwo.js
@@ -2,9 +2,19 @@ import React from "react";
 import { SectionTwoDiv } from "../styles/styledDashboard";
 import { Item } from "../pages/dashboard";
 import styled from "@emotion/styled";
-import { Avatar, Badge, Paper, Stack, Typography } from "@mui/material";
+import { Avatar, Badge, Paper, Typography } from "@mui/material";
 import { deepOrange } from "@mui/material/colors";
 import NotificationsIcon from "@mui/icons-material/Notifications";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import IconButton from "@mui/material/IconButton";
+import FolderIcon from "@mui/icons-material/Folder";
+import DeleteIcon from "@mui/icons-material/Delete";
+import ArticleIcon from "@mui/icons-material/Article";
+import ContentCopyIcon from "@mui/icons-material/ContentCopy";
 
 const Profile = styled(Paper)(({ theme }) => ({
   ...theme.typography.body2,
@@ -25,6 +35,13 @@ const ProfileDetails = styled(Paper)(({ theme }) => ({
   margin: "10px auto",
   padding: "10px",
   lineHeight: "60px",
+}));
+
+const Demo = styled("div")(({ theme }) => ({
+  backgroundColor: theme.palette.background.paper,
+  width: "100%",
+  height: "100%",
+  overflowY: "scroll",
 }));
 
 export const SectionTwo = () => {
@@ -52,7 +69,31 @@ export const SectionTwo = () => {
             </Typography>
           </div>
         </Profile>
-        <ProfileDetails variant="outlined"></ProfileDetails>
+        <ProfileDetails variant="outlined">
+          <Demo>
+            <List>
+              <ListItem>
+                <ListItemAvatar>
+                  <ListItemIcon>
+                    <FolderIcon />
+                  </ListItemIcon>
+                </ListItemAvatar>
+                <ListItemText primary="Single-line item" />
+                <React.Fragment>
+                  <IconButton sx={{ mx: { xs: "1x", sm: 1, md: 1 } }}>
+                    <DeleteIcon sx={{ fontSize: "30px" }} />
+                  </IconButton>
+                  <IconButton sx={{ mx: { xs: "1px", sm: 1, md: 1 } }}>
+                    <ArticleIcon sx={{ fontSize: "30px" }} />
+                  </IconButton>
+                  <IconButton sx={{ mx: { xs: "1px", sm: 1, md: 1 } }}>
+                    <ContentCopyIcon sx={{ fontSize: "30px" }} />
+                  </IconButton>
+                </React.Fragment>
+              </ListItem>
+            </List>
+          </Demo>
+        </ProfileDetails>
       </Item>
     </SectionTwoDiv>
   );
