1cbef27811c601c23e500ceb337f53611eb1715a
JohnnyOhms <officialjohn662@gmail.com>
2023-11-18 08:53:06 -0500

    triggered render of the sectionTwo component to load and displayed the saved text

--- src/component/Result/resultText.js
+++ src/component/Result/resultText.js
@@ -5,7 +5,9 @@ import SaveAltIcon from "@mui/icons-material/SaveAlt";
 import ClearIcon from "@mui/icons-material/Clear";
 import styled from "@emotion/styled";
 import { useDispatch } from "react-redux";
-import { openResult } from "../../slice/globalSlice";
+import { openResult, reRenderComponent } from "../../slice/globalSlice";
+import { formateDate } from "../../utils/date";
+import { saveText } from "../../slice/textSlice";
 
 const Result = styled(Paper)(({ theme }) => ({
   ...theme.typography.body2,
@@ -20,6 +22,14 @@ const Result = styled(Paper)(({ theme }) => ({
 
 export const ResultText = ({ text }) => {
   const dispatch = useDispatch();
+
+  const handleSaveClick = (e) => {
+    dispatch(saveText({ text, date: formateDate() }));
+    dispatch(openResult());
+    //re render sectionTwo component
+    dispatch(reRenderComponent());
+  };
+
   return (
     <Box
       sx={{
@@ -51,7 +61,7 @@ export const ResultText = ({ text }) => {
               <ContentCopyIcon />
             </IconButton>
 
-            <IconButton>
+            <IconButton onClick={handleSaveClick}>
               <SaveAltIcon />
             </IconButton>
           </span>
